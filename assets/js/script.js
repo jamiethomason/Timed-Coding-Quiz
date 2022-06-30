75c6c42881aa5dc05ab6636c3820efe4b5d75f14
Your Name <JamieLou@jamie-revills-macbook.local>
2022-06-30 10:00:49 -0400

    styling buttons

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+// Question Arrays
+var questions = ['Commonly used data types DO Not Include:', 'The condition in an if / else statement is enclosed with _____.', 'Arrays in JavaScript can be used to store _____.', 'String values must be enclosed within _____ when being assigned to variables.', 'A very useful tool used during development and debugging for printing content to the debugger is:'];
+var answerSet1 = ['strings', 'booleans', 'alerts', 'numbers'];
+var answerSet2 = ['quotes', 'curly brackets', 'parenthesis', 'square brackets'];
+var answerSet3 = ['numbers and strings', 'other arrays', 'booleans', 'all of the above'];
+var answerSet4 = ['commas', 'curly brackets', 'quotes', 'parenthesis'];
+var answerSet5 = ['JavaScript', 'terminal/bash', 'for loops', 'console.log'];
